c70e2b0c42e33d0feede459ee47ec2bb86b41d7b
kei-1111 <kei_02_1111@icloud.com>
2025-11-04 20:57:58 +0900

    chore: :core:designsystemへ移動したテーマを使用するように変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     detektPlugins(libs.detekt.formatting)
     detektPlugins(libs.detekt.compose)
 
+    implementation(projects.core.designsystem)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
