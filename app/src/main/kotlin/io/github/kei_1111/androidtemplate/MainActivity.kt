325922b77d8e2482d7510ff61813f46169de8352
kei-1111 <kei_02_1111@icloud.com>
2025-11-05 00:03:28 +0900

    chore: detektを実行しフォーマット整理

--- app/src/main/kotlin/io/github/kei_1111/androidtemplate/MainActivity.kt
+++ app/src/main/kotlin/io/github/kei_1111/androidtemplate/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidTemplateTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
                 }
             }
         }
